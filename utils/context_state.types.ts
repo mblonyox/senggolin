c6fbdde0046e0521d0af18f5fe291e32ad12bdc9
Sukirno <mblonyox@gmail.com>
2023-03-22 14:08:47 +0700

    fix: add Database interface

--- utils/context_state.types.ts
+++ utils/context_state.types.ts
@@ -1,6 +1,125 @@
 import { SupabaseClient, User } from "@supabase/supabase-js";
 
+interface Database {
+  public: {
+    Tables: {
+      click: {
+        Row: {
+          created_at: string;
+          id: number;
+          link_id: number;
+          remote_ip: string;
+        };
+        Insert: {
+          created_at?: string;
+          id?: number;
+          link_id: number;
+          remote_ip: string;
+        };
+        Update: {
+          created_at?: string;
+          id?: number;
+          link_id?: number;
+          remote_ip?: string;
+        };
+      };
+      link: {
+        Row: {
+          created_at: string;
+          icon_url: string;
+          id: number;
+          page_id: number;
+          text: string;
+          url: string;
+        };
+        Insert: {
+          created_at?: string;
+          icon_url: string;
+          id?: number;
+          page_id: number;
+          text: string;
+          url: string;
+        };
+        Update: {
+          created_at?: string;
+          icon_url?: string;
+          id?: number;
+          page_id?: number;
+          text?: string;
+          url?: string;
+        };
+      };
+      page: {
+        Row: {
+          created_at: string;
+          description: string;
+          id: number;
+          slug: string;
+          title: string;
+          user_id: string;
+        };
+        Insert: {
+          created_at?: string;
+          description: string;
+          id?: number;
+          slug: string;
+          title: string;
+          user_id: string;
+        };
+        Update: {
+          created_at?: string;
+          description?: string;
+          id?: number;
+          slug?: string;
+          title?: string;
+          user_id?: string;
+        };
+      };
+      visit: {
+        Row: {
+          created_at: string;
+          id: number;
+          page_id: number;
+          remote_ip: string;
+        };
+        Insert: {
+          created_at?: string;
+          id?: number;
+          page_id: number;
+          remote_ip: string;
+        };
+        Update: {
+          created_at?: string;
+          id?: number;
+          page_id?: number;
+          remote_ip?: string;
+        };
+      };
+    };
+    Views: {
+      page_views: {
+        Row: {
+          count: number | null;
+          id: number | null;
+          slug: string | null;
+          title: string | null;
+          user_id: string | null;
+        };
+      };
+    };
+    Functions: {
+      [_ in never]: never;
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
+}
+
 export type ContextState = {
-  supabaseClient?: SupabaseClient;
+  supabaseClient?: SupabaseClient<Database>;
   user?: User;
 };
